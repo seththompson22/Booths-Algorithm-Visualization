7e9d716bb8f8b667ddc1235e32d757181dd9efe8
Seth Thompson <thompsonrussell22@gmail.com>
2023-09-07 23:34:59 -0400

    updating algorithm for function

--- app.py
+++ app.py
@@ -6,8 +6,23 @@ from flask import Flask, render_template, request
 # Provide template folder name
 # The default folder name should be "templates" else need to mention custom folder name
 app = Flask(__name__)
- 
+
+def ASR(R, A, Aextra):
+    pass
+
 def visualize_booth(decimal_multiplicand, decimal_multiplier):
+    A = decimal_multiplicand
+    C = decimal_multiplier
+    Aextra=0
+    R=0
+    n=4
+    while (n>0):
+        if (A&0x1!= Aextra):
+            if (Aextra==0):
+                R=R-C
+            else:
+                R=R+C
+        ASR(R, A, Aextra)
     return render_template("visualize.html")
 
 # @app.route('/')
